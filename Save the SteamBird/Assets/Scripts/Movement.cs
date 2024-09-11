44a0e737efb109150952d0ddaecef563436f1df7
CheesyMoon <90181881+MitchelBroekate@users.noreply.github.com>
2024-09-11 10:19:34 +0200

    Movement + new input system
    -added the new input system
    -added movement script

---
+++ Save the SteamBird/Assets/Scripts/Movement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Control controls;
+    Rigidbody rb;
+
+    [SerializeField] float moveSpeed;
+    [SerializeField] Vector2 movementInput;
+
+    private void Start()
+    {
+        controls = new Control();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        movementInput = controls.Player.Movement.ReadValue<Vector2>();
+
+        rb.velocity = movementInput * moveSpeed;
+
+    }
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+}
