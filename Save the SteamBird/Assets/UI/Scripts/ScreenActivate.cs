2de2ff950e6d06a3b2e95466f3d72ebf8059acc7
MacKenzie1976 <97097945@st.deltion.nl>
2024-09-23 16:59:38 +0200

    Linking

---
+++ Save the SteamBird/Assets/UI/Scripts/ScreenActivate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenActivate : MonoBehaviour
+{
+
+    public GameObject winCanvas;
+    public GameObject loseCanvas;
+
+    public void SetWinActive()
+    {
+       // gameObject.SetActive
+    }
+
+
+}
