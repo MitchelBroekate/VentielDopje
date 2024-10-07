235657979796a0f9588a0989ceaad5b97ca041e7
MacKenzie1976 <97097945@st.deltion.nl>
2024-10-07 12:33:12 +0200

    Lighting

--- Save the SteamBird/Assets/Scripts/Cog Puzzle/CogRotate.cs
+++ Save the SteamBird/Assets/Scripts/Cog Puzzle/CogRotate.cs
@@ -6,19 +6,24 @@ public class CogRotate : MonoBehaviour
 {
     public bool canRotate;
     public bool rotateDir;
+  
 
     void FixedUpdate()
     {
 
         if(canRotate)
         {
-            if(rotateDir)
+            if (rotateDir)
             {
                 transform.Rotate(new Vector3(0, 3, 0));
+                
+                
             }
             else
             {
                 transform.Rotate(new Vector3(0, -3, 0));
+               
+                
             }
         }
     }
