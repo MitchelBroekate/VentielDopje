55519d739f1ff967de4b90904a85ff90c1f7b5ca
MitchelBroekate <mitchelb11@outlook.com>
2024-10-15 15:44:18 +0200

    dev tools

---
+++ Save the SteamBird/Assets/Scripts/Player/DevTools.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DevTools : MonoBehaviour
+{
+    public Transform birdpart1, birdpart2, birdpart3, birdpart4;
+
+    public Timer timer;
+
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            birdpart1.transform.position = transform.position;
+            birdpart2.transform.position = transform.position;
+            birdpart3.transform.position = transform.position;
+            birdpart4.transform.position = transform.position;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            timer.timeRemaining = 1;
+        }
+    }
+}
