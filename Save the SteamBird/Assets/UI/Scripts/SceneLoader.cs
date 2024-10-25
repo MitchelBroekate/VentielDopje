972d7bea3e218efd7e7f2bf0fdf8758a961059cd
Hvtende <97097945@st.deltion.nl>
2024-10-25 12:39:18 +0200

    LoadingScreen

---
+++ Save the SteamBird/Assets/UI/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider loadingBar;
+
+
+
+    public void SceneLoad(int levelIndex)
+    {
+        StartCoroutine(LoadSceneAsynchronously(levelIndex));
+    }
+
+    IEnumerator LoadSceneAsynchronously(int levelIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelIndex);
+        loadingScreen.SetActive(true);
+        while(!operation.isDone)
+        {
+            loadingBar.value = operation.progress;
+            yield return null;
+        }
+    }
+}
